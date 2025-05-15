3945ead67d1ec65f3d9a36bccd988baea95c3c8d
MatejAlbrecht <95190233+MatejAlbrecht@users.noreply.github.com>
2025-05-15 16:44:47 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -28,19 +28,18 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
+
     try:
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
         print('Exception')
 
-
 def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
